310db2c836b72c614425194d7c9a24e996e65e54
lmpec <107778023+lmpec@users.noreply.github.com>
2025-05-14 13:48:25 -0700

    New edit

--- hello-world-paige-j.c
+++ hello-world-paige-j.c
@@ -2,6 +2,8 @@
 
 int main() {
     std::cout << "Hello World! by Paige J." << std::endl;
+    std::cout << " this edit is done by Evelin." << std::endl;
+
     return 0;
 }
 // This is a simple C++ program that prints "Hello World!" to the console.
