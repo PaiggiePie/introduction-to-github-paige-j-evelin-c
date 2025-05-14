cae5b0415168fa55dbb7f0f344a6820b8a896964
PaiggiePie <paiggiepie@gmail.com>
2025-05-14 13:38:07 -0700

    Update hello-world-paige-j.c
    added name

--- hello-world-paige-j.c
+++ hello-world-paige-j.c
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello World!";
+    std::cout << "Hello World! by Paige J." << std::endl;
     return 0;
 }
 // This is a simple C++ program that prints "Hello World!" to the console.
